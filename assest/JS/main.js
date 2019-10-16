8d59bd4860622a1e6fee8cb862a47f607b064c87
1Raghad1 <raghadallhyani@gmail.com>
2019-10-16 09:57:39 +0300

    fix some problems

--- assest/JS/main.js
+++ assest/JS/main.js
@@ -4,25 +4,31 @@ var row = 3;
 var col = 3;
 var score = 0;
 var count = 0;
-var numOfColred=3;
+var numOfColred = 3;
 var setOfSol;
 
 function creatEmptyBored() {
+
     gameBord.innerHTML = ''
+
     for (let i = 0; i < row; i++) {
+
         for (j = 0; j < col; j++) {
 
             let gridItem = document.createElement('div');
             gridItem.setAttribute("data-id", "" + i + j);
             gridItem.setAttribute("id", "" + i + j);
             gridItem.setAttribute('class', 'gridItem');
-            if(row==4){
-                gridItem.style.width='100px';
-                gridItem.style.height='100px';
+
+            if (row == 4) {
+
+                gridItem.style.width = '100px';
+                gridItem.style.height = '100px';
             }
-            if(row==5){
-                gridItem.style.width='70px';
-                gridItem.style.height='70px';
+            if (row == 5) {
+
+                gridItem.style.width = '70px';
+                gridItem.style.height = '70px';
 
             }
             gameBord.appendChild(gridItem)
@@ -37,55 +43,73 @@ function colorBored() {
     let time = 500;
 
     for (let i = 0; i < colored.length; i++) {
-        cell[i] = document.getElementById(colored[i])
-        time += 400;
-        setTimeout(function () { id = setInterval(colorSeq(cell[i]), time); }, time);
 
-        cell[i].classList.add('animated', 'fadeIn')
+        cell[i] = document.getElementById(colored[i])
+        time += 800;
+        setTimeout(function () { id = setInterval(colorSeq(cell[i]), time); }, time);
+      
         setOfCells.push(cell[i].getAttribute('data-id'))
     }
 
     function colorSeq(cell) {
+
         cell.style.backgroundColor = '#2d2c40'
     }
-
-
 
     return checkforSolution(setOfCells)
 }
 function random() {
-    {
-
-        let arr2 = []
-        let obj = [[],[],[],[],[]];
-        for(let i=0;i<row;i++){
-            for(let j=0;j<col;j++)
-            obj[i][j]=""+i+j
-        }
 
 
-        while (arr2.length != numOfColred) {
-            let i = Math.floor(Math.random() * row)
-            let j=Math.floor(Math.random() * row)
-           if (!arr2.includes(obj[i][j])) {
-                arr2.push(obj[i][j])
-            }
-        }
-        return arr2
+    let arr2 = []
+    let obj = [[], [], [], [], []];
+    for (let i = 0; i < row; i++) {
+
+        for (let j = 0; j < col; j++)
+
+            obj[i][j] = "" + i + j
     }
+
+
+    while (arr2.length != numOfColred) {
+
+        let i = Math.floor(Math.random() * row)
+        let j = Math.floor(Math.random() * row)
+
+        if (!arr2.includes(obj[i][j])) {
+
+            arr2.push(obj[i][j])
+        }
+    }
+    return arr2
 }
 
+
 function checkforSolution(sol) {
+
     let allCell = document.getElementsByClassName('gridItem');
     let subSet = [];
-   
+
     for (let i = 0; i < allCell.length; i++) {
 
         if (sol.includes(allCell[i].getAttribute('data-id'))) {
-
             allCell[i].addEventListener('click', function (e) {
+            allCell[i].classList.add('animated', 'fadeIn')
+            allCell[i].classList.add('animated', 'fast')
+            if(subSet.includes(allCell[i].getAttribute('data-id'))){
+                Swal.fire({
+                    type: 'error',
+                    title: 'Oops...',
+                    text: 'You Can Not Choose The SAME Square Twice!!',
+               
+                  })
+                  creatEmptyBored();
+            }else{
                 subSet.push(allCell[i].getAttribute('data-id'))
-                count++;
+            }
+            
+               
+               
                 if (subSet.length === sol.length) {
 
                     check(subSet);
@@ -93,7 +117,9 @@ function checkforSolution(sol) {
                 }
 
             })
+
         } else {
+
             allCell[i].addEventListener('click', function (e) {
                 allCell[i].style.opacity = '0.4'
                 allCell[i].classList.add('animated', 'shake')
@@ -107,33 +133,70 @@ function checkforSolution(sol) {
             subSet = subSet.toString();
 
             if (subSet === sol) {
-                score++;
-                
-var scor = document.getElementsByTagName('span')
-scor[0].innerHTML=score;
+                score++
+                displayScore(score);
+                if (score <= 2) {
+                    if (score % 3 == 0 || score == 1) {
 
-if(score%3==0||score==1||score>6&&score<=10){
+                        numOfColred++;
+                        creatEmptyBored();
 
-    numOfColred++;
-    
-    }else{
+                    } else {
 
-        if(row<5){
+                        if (row < 5) {
 
-        row++;
-        col++;
+                            row++;
+                            col++;
+                            creatEmptyBored();
+                        }
+
+                    }
+
+
+                } else {
+
+                    Swal.fire(
+                        'Good job!',
+                        'You reached 10 Scores!!',
+                        'success'
+                      )
+
+                }
+            } else {
+                creatEmptyBored()
+
+                if (score > 0)
+
+                    score--;
+
+                displayScore(score)
+
+            }
+
         }
-        
     }
-     creatEmptyBored();
+}
 
-            }else{
-            creatEmptyBored()
-              
-           }
-        }
-        }
-    }
+
+let again = document.getElementById('again');
+again.addEventListener('click', function () {
+    creatEmptyBored();
+})
+
+let reset = document.getElementById('reset');
+reset.addEventListener('click', function () {
+    score = 0;
+    displayScore(score);
+    row = 3;
+    col = 3;
+    numOfColred = 3;
+    creatEmptyBored();
+
+})
+
+function displayScore(score) {
+    var scor = document.getElementsByTagName('span')
+    scor[0].innerHTML = score;
+}
 
 creatEmptyBored();
-
